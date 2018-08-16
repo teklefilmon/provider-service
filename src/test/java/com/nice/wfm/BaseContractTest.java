99dde58355c26fccaf275ca033404d881a7a5dce
Filmon Tekle <teklefilmon@yahoo.com>
2018-08-16 16:30:49 -0500

    add base contract test class

---
+++ src/test/java/com/nice/wfm/BaseContractTest.java
@@ -0,0 +1,26 @@
+package com.nice.wfm;
+
+import com.nice.wfm.controller.CreditCheckController;
+import com.nice.wfm.dto.CreditScoreResponse;
+import com.nice.wfm.service.CreditCheckService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created on 8/16/18.
+ * Author: filmon
+ * Apple Inc.
+ */
+
+public class BaseContractTest {
+
+    @Before
+    public void setup(){
+        final CreditCheckService creditCheckService = mock(CreditCheckService.class);
+        when(creditCheckService.doCreditCheck("12-34-5678")).thenReturn(new CreditScoreResponse(CreditScoreResponse.CreditScore.HIGH));
+        RestAssuredMockMvc.standaloneSetup(new CreditCheckController(creditCheckService));
+    }
+}
